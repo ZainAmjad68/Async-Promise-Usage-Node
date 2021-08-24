227fd9e35cef3b229ff39917ac2a752201e456df
ZainAmjad68 <zainamjad68@yahoo.com>
2021-08-24 15:22:13 +0500

    1st Commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-// haha this is a comment
+
 const clothing = require('./server/routes/clothing');
 const errors = require('./server/routes/errors');
 
